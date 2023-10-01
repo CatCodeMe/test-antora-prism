388c91b18b0c869603b58273a39f4ceeb77ba49d
hulinjiang <hulinjiang@58.com>
2023-10-01 10:54:59 +0800

    add start script

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const http = require('http');
 const serveStatic = require('serve-static');
 
-// 静态服务器
 var serve = serveStatic('build/site', { index: ['index.html', 'index.htm'] })
 
 // Create server
-//var server = http.createServer(function onRequest(req, res) {
-//        serve(req, res, () => { });
-//})
-//
-//// Listen
-//server.listen(8001, () => {
-//        console.log("open http://localhost:3000")
-//});
+var server = http.createServer(function onRequest(req, res) {
+        serve(req, res, () => { });
+})
+
+// Listen
+server.listen(8001, () => {
+        console.log("open http://localhost:8001")
+});
 
